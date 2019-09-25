60b33999d6eae5e419ac122679b833f5c62ec65d
Jay <djayshine87@hotmail.com>
2019-09-25 15:27:21 +1000

    Updates

---
+++ precache-manifest.4a28a3a0ee0965e0238453c85c916b3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c681cdbe2a02ab0c8b3bdaed57b5ca1",
+    "url": "/myvideo/index.html"
+  },
+  {
+    "revision": "a844b898d37407a7f110",
+    "url": "/myvideo/static/css/main.a5b1a586.chunk.css"
+  },
+  {
+    "revision": "078aaca0dc525ab91b15",
+    "url": "/myvideo/static/js/2.891b848a.chunk.js"
+  },
+  {
+    "revision": "a844b898d37407a7f110",
+    "url": "/myvideo/static/js/main.714e432a.chunk.js"
+  },
+  {
+    "revision": "b309c39a3358d8e43aaa",
+    "url": "/myvideo/static/js/runtime-main.c1c76c5b.js"
+  }
+]);
